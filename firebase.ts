f0662f6b7ab1f6be2eb640fe8cc89b147e820e90
Tomas Jay <tomasj21481@gmail.com>
2025-02-10 16:50:57 -0500

    initialize firestore and create env

--- firebase.ts
+++ firebase.ts
@@ -1,21 +1,22 @@
-// Import the functions you need from the SDKs you need
+// filepath: /c:/Users/Tomas/OneDrive/Projects/react-native-anki/flashcards-app/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC7NI_2R-dgWw5F32SiS8PYAWrIdkSZtfc",
-  authDomain: "react-native-anki.firebaseapp.com",
-  projectId: "react-native-anki",
-  storageBucket: "react-native-anki.firebasestorage.app",
-  messagingSenderId: "954417309791",
-  appId: "1:954417309791:web:9c04397642ddb6df6453ea",
-  measurementId: "G-SYRXFH648N"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+
+export { app, analytics, auth, firestore };
